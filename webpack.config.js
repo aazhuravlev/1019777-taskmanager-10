9af84c4f8d00a1cd25b413c1322cfd7eaccc9df4
Aleksandr <zhuravlev.a.a@me.com>
2019-12-01 00:28:19 +0300

    заканчивает задание по структуре данных

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    publicPath: `http://localhosst:8080/`,
     compress: true,
     watchContentBase: true
   }
