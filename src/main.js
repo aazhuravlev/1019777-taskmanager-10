222dd7be588562050fb8797541c77c0e640898da
Aleksandr <zhuravle.a.a@me.com>
2020-01-22 18:39:50 +0300

    добавляет выполнение задания

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import API from './api.js';
+import Api from './api/index.js';
+import Store from './api/store.js';
+import Provider from './api/provider.js';
 import BoardComponent from './components/board.js';
 import BoardController from './controllers/board.js';
 import FilterController from './controllers/filter.js';
@@ -7,13 +9,18 @@ import StatisticsComponent from './components/statistics.js';
 import TasksModel from './models/tasks.js';
 import {render, createFragment, RenderPosition} from './utils/render.js';
 
+const STORE_PREFIX = `taskmanager-localstorage`;
+const STORE_VER = `v1`;
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
 const AUTHORIZATION = `Basic dXNlckBwYZFad28yAo=`;
 const END_POINT = `https://htmlacademy-es-10.appspot.com/task-manager`;
 
 const tasksModel = new TasksModel();
 
 const pasteElements = () => {
-  const api = new API(END_POINT, AUTHORIZATION);
+  const store = new Store(STORE_NAME, window.localStorage);
+  const api = new Api(END_POINT, AUTHORIZATION);
+  const apiWithProvider = new Provider(api, store);
   const siteMainElement = document.querySelector(`.main`);
   const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
   const siteMenuComponent = new SiteMenuComponent();
@@ -34,7 +41,7 @@ const pasteElements = () => {
   const boardComponent = new BoardComponent();
   render(siteMainElement, createFragment([boardComponent.getElement(), statisticsComponent.getElement()]), RenderPosition.BEFOREEND);
 
-  const boardController = new BoardController(boardComponent, tasksModel, api);
+  const boardController = new BoardController(boardComponent, tasksModel, apiWithProvider);
 
   statisticsComponent.hide();
 
@@ -56,10 +63,38 @@ const pasteElements = () => {
         break;
     }
   });
-  api.getTasks()
+
+  apiWithProvider.getTasks()
   .then((tasks) => {
     tasksModel.setTasks(tasks);
     boardController.render();
+  });
+
+  window.addEventListener(`load`, () => {
+    navigator.serviceWorker.register(`/sw.js`)
+      .then(() => {
+        // Действие, в случае успешной регистрации ServiceWorker
+      }).catch(() => {
+        // Действие, в случае ошибки при регистрации ServiceWorker
+      });
+  });
+
+  window.addEventListener(`online`, () => {
+    document.title = document.title.replace(` [offline]`, ``);
+
+    if (!apiWithProvider.getSynchronize()) {
+      apiWithProvider.sync()
+        .then(() => {
+          // Действие, в случае успешной синхронизации
+        })
+        .catch(() => {
+          // Действие, в случае ошибки синхронизации
+        });
+    }
+  });
+
+  window.addEventListener(`offline`, () => {
+    document.title += ` [offline]`;
   });
 };
 
